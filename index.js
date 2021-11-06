239f226a5bb194a1c855bf5d4cc765da50265103
Ryan Dellolio <ryan@dellol.io>
2021-11-06 14:24:06 -0400

    Feature/polling (#1)
    * working dynamic bg
    
    * working hello world polling
    
    * Update index.js
    
    * Update index.js
    
    * Revert "Update index.js"
    
    This reverts commit f81df13015995fae7b00db022fc3470b3e7d08dc.
    
    * Revert "Update index.js"
    
    This reverts commit 8880a83cbd32c46480d6c883445bdfb149454de6.
    
    * working poller and dynamic bg feed.

--- index.js
+++ index.js
@@ -4,24 +4,45 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const axios = require("axios");
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+var sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+var poll = (promiseFn, time) =>
+  promiseFn().then(sleep(time).then(() => poll(promiseFn, time)));
 
-var sgv = 153;
-
+// Greet the World every second
+poll(
+  () =>
+    new Promise(() => {
+      // Make a request for a user with a given ID
+      axios
+        .get("https://glucose.ryan.dellol.io/api/v1/entries/current.json")
+        .then(function (response) {
+          // handle success
+          console.log(response.data[0].sgv + ' at ' + response.data[0].dateString);
+          io.emit("sgv", response.data[0].sgv);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
+    }),
+  2000
+);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-
-    console.log("sgv: " + sgv);
-    io.emit('sgv', sgv);
-  
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(3000, () => {
