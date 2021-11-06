be1ad6fd4ee4fb29ee476376348f3d13d095ed57
Ryan Dellolio <ryan@dellol.io>
2021-11-06 10:09:41 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+var sgv = 153;
+
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+
+    console.log("sgv: " + sgv);
+    io.emit('sgv', sgv);
+  
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
