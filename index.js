d7123334d0ace4da130d8d68c7ce442a411772f4
Ryan Dellolio <ryan@dellol.io>
2021-11-06 10:19:27 -0400

    working dynamic bg

--- index.js
+++ index.js
@@ -4,26 +4,36 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const axios = require("axios");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-var sgv = 153;
+// Make a request for a user with a given ID
+axios
+  .get("https://glucose.ryan.dellol.io/api/v1/entries/current.json")
+  .then(function (response) {
+    // handle success
+    console.log(response.data[0].sgv);
 
+    io.on("connection", (socket) => {
+      console.log("a user connected");
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+      io.emit("sgv", response.data[0].sgv);
 
-
-    console.log("sgv: " + sgv);
-    io.emit('sgv', sgv);
-  
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+      socket.on("disconnect", () => {
+        console.log("user disconnected");
+      });
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
   });
-});
-
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
