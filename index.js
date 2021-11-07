3fd3881d694d3c76aa98d606b69126f8085e3ffa
Ryan Dellolio <ryan@dellol.io>
2021-11-06 20:45:38 -0400

    Feature/emit on change (#2)
    * Working!
    
    * Update index.js

--- index.js
+++ index.js
@@ -19,25 +19,32 @@ poll(
         .get("https://glucose.ryan.dellol.io/api/v1/entries/current.json")
         .then(function (response) {
           // handle success
-          console.log(response.data[0].sgv + ' at ' + response.data[0].dateString);
-          io.emit("sgv", response.data[0].sgv);
+
+
+          if (global.dateString !== response.data[0].dateString) {
+            console.log("New reading detected");
+            io.emit("sgv", response.data[0].sgv);
+          }
+          global.dateString = response.data[0].dateString;
+
+          console.log(
+            response.data[0].sgv + " at " + response.data[0].dateString
+          );
         })
+
         .catch(function (error) {
           // handle error
           console.log(error);
-        })
-        .then(function () {
-          // always executed
         });
     }),
-  2000
+  4000
 );
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("User " + socket.id + " connected");
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("User " + socket.id + " disconnected");
   });
 });
 
