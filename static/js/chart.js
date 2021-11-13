7883789f7f2f94cedfa80cf8892e4f3a0a74bf06
Ryan Dellolio <ryan@dellol.io>
2021-11-13 12:04:56 -0500

    working prototype

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,1 +1,22 @@
-hi
+const ctx = document.getElementById("myChart");
+const myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Glucose mg/dl",
+        data: [],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+
