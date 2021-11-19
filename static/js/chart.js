31d3a1eaf85ec89bee4bed7a5ac2c37095ea875e
Ryan Dellolio <ryan@dellol.io>
2021-11-18 22:01:48 -0500

    basic proof of concept

--- static/js/chart.js
+++ static/js/chart.js
@@ -7,8 +7,8 @@ const myChart = new Chart(ctx, {
       {
         label: "Glucose mg/dl",
         borderDash: [5, 5],
-        borderColor: "#bae755",
-        backgroundColor: "#bae755",
+        borderColor: "#000",
+        backgroundColor: "#000",
         data: [],
       },
     ],
@@ -16,11 +16,27 @@ const myChart = new Chart(ctx, {
   options: {
     scales: {
       y: {
+        max: 200,
+        min: 30,
         beginAtZero: true,
         ticks: {
           // Include a dollar sign in the ticks
           callback: function (value, index, values) {
             return value + " mg/dL";
+          },
+        },
+      },
+    },
+    plugins: {
+      autocolors: false,
+      annotation: {
+        annotations: {
+          line1: {
+            type: "line",
+            yMin: 60,
+            yMax: 60,
+            borderColor: "rgb(255, 99, 132)",
+            borderWidth: 2,
           },
         },
       },
