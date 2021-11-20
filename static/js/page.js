789d53c31bd936a13a979d73aff8b6e814199147
Ryan Dellolio <ryan@dellol.io>
2021-11-20 13:24:15 -0500

    WORKING but no scroll effect

--- static/js/page.js
+++ static/js/page.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
 
   socket.on("retro", function (history) {
     i = history.length - 1;
-
+    myChart.data.datasets[1].data.push(null); //we don't know what came before the retro values
+    
     while (i > 0) {
       myChart.data.labels.push(moment(history[i].dateString).format("hh:mm a"));
       myChart.data.datasets[0].data.push(history[i].sgv);
@@ -26,9 +27,11 @@ $(document).ready(function () {
 
     //remove the last reading each new reading to create the scroll effect
     //if the reading is over 200, update the axis
-    myChart.data.labels.splice(0, 1);
-    myChart.data.datasets[0].data.splice(0, 1);
-    myChart.data.datasets[1].data.splice(0, 1);
+    //myChart.data.labels.splice(0, 1);
+    //myChart.data.datasets[0].data.splice(0, 1);
+    //myChart.data.datasets[1].data.splice(0, 1);
+
+    //^^^THIS IS BROKEN
 
     if (msg.sgv > 200) {
       myChart.options.scales.y.max = 400;
@@ -39,11 +42,14 @@ $(document).ready(function () {
     myChart.data.labels.push(moment(msg.dateString).format("hh:mm a"));
     myChart.data.datasets[0].data.push(msg.sgv);
 
-    twoback = myChart.data.datasets[0].data.slice(-2)[0]; //go two back to calculate live delta
-    var delta = twoback - msg.sgv;
-    myChart.data.datasets[1].data.push(-delta);
+    twoback = myChart.data.datasets[0].data.slice(-2)[0];
+    var delta = msg.sgv - twoback;
+    myChart.data.datasets[1].data.push(delta);
 
-    console.log(myChart);
+    console.log(myChart.data.labels);
+    console.log(myChart.data.datasets[0].data);
+    console.log(myChart.data.datasets[1].data);
+
     myChart.update(); //update chart
     $(document).prop("title", generateTitle(msg, delta)); //update title
   });
