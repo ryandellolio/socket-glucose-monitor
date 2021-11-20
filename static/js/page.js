7c72a41da9db758d051a7bcb6772184034144caf
Ryan Dellolio <ryan@dellol.io>
2021-11-20 14:19:26 -0500

    Logic tweak!

--- static/js/page.js
+++ static/js/page.js
@@ -7,10 +7,10 @@ $(document).ready(function () {
     i = history.length - 1;
     myChart.data.datasets[1].data.push(null); //we don't know what came before the retro values
 
-    while (i > 0) {
+    while (i != 0) {
       myChart.data.labels.push(moment(history[i].dateString).format("hh:mm a"));
       myChart.data.datasets[0].data.push(history[i].sgv);
-      if (i != history.length - 1) {
+      if (i != 1) {
         //don't show the last delta lest it gets repeated in the reading display
         myChart.data.datasets[1].data.push(history[i - 1].sgv - history[i].sgv);
       }
@@ -44,15 +44,17 @@ $(document).ready(function () {
 
     //remove the last reading each new reading to create the scroll effect
     //if the reading is over 200, update the axis
-    //myChart.data.labels.splice(0, 1);
-    //myChart.data.datasets[0].data.splice(0, 1);
-    //myChart.data.datasets[1].data.splice(0, 1);
+    myChart.data.labels.shift();
+    myChart.data.datasets[0].data.shift();
+    myChart.data.datasets[1].data.shift();
 
     //^^^THIS IS BROKEN
 
     console.log(myChart.data.labels);
     console.log(myChart.data.datasets[0].data);
     console.log(myChart.data.datasets[1].data);
+    console.log("Delta: " + delta);
+
 
     myChart.update(); //update chart
     $(document).prop("title", generateTitle(msg, delta)); //update title
